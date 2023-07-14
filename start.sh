c8e781b84e96bd8664dbd8c3ddd1cac2710519a2
shiv-dwivedi <115983642+shiv-dwivedi@users.noreply.github.com>
2023-07-14 20:20:03 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/searchingshiv/KD-URL.git /KD-URL
+  git clone https://github.com/shiv-dwivedi/KD.git /KD
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /KD-URL
+  git clone $UPSTREAM_REPO /KD
 fi
 cd /KD-URL
 pip3 install -U -r requirements.txt
