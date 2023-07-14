e5ff93ab92144c1e1c5672d01faac4b86533c825
shiv-dwivedi <115983642+shiv-dwivedi@users.noreply.github.com>
2023-07-14 20:22:59 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /KD
 fi
-cd /KD-URL
+cd /KD
 pip3 install -U -r requirements.txt
 echo "Starting Lucifer...."
 python3 bot.py
